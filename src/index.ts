35d3c2577f03cadef20014eb76579eded8429671
pratham_srnk <pratham@appointy.com>
2023-04-25 15:39:19 +0530

    Changed index.ts (unstable)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello!');
+    res.send('Hello world from unstable branch!');
 });
 
 app.listen(3000, () => {
